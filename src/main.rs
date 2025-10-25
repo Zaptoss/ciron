3e8dc76e64253e8a56ace56f3217141497408f61
Zaptoss <zaptosgg@gmail.com>
2025-10-25 15:49:00 +0300

    WIP: Config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+use config::load_config;
+
+fn main() -> anyhow::Result<()> {
+    let config = load_config("ciron.toml")?;
+    println!("{:#?}", config);
+
+    Ok(())
 }
